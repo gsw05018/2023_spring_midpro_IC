3ada5674e3ab2aa0bf69877778c4a9c95aee65b6
한인철 <gsw05018@naver.com>
2023-11-17 06:31:31 +0900

    fix: 알림스타일 toast로 변경

--- midProject/src/main/resources/static/common.js
+++ midProject/src/main/resources/static/common.js
@@ -1,4 +1,6 @@
 
+
+
 // 로딩 팝업 창
 window.onload = function () {
     var modal = document.getElementById("imageModal");
@@ -123,3 +125,52 @@ $(document).ready(function () {
 });
 
 // 올림버튼 끝
+
+   // toast 알림
+toastr.options = {
+    closeButton: true,
+    debug: false,
+    newestOnTop: true,
+    progressBar: true,
+    positionClass: "toast-top-right",
+    preventDuplicates: false,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    timeOut: "5000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut"
+};
+
+function parseMsg(msg) {
+    const [pureMsg, ttl] = msg.split(";ttl=");
+
+    const currentJsUnixTimestamp = new Date().getTime();
+
+    if (ttl && parseInt(ttl) + 5000 < currentJsUnixTimestamp) {
+        return [pureMsg, false];
+    }
+
+    return [pureMsg, true];
+}
+
+function toastNotice(msg) {
+    const [pureMsg, needToShow] = parseMsg(msg);
+
+    if (needToShow) {
+        toastr["success"](pureMsg, "알림");
+    }
+}
+
+function toastWarning(msg) {
+    const [pureMsg, needToShow] = parseMsg(msg);
+
+    if (needToShow) {
+        toastr["warning"](pureMsg, "경고");
+    }
+}
+
+
