eb904b2cd4f90a26dffee470c5fb0b8f74698027
한인철 <gsw05018@naver.com>
2023-12-12 15:39:25 +0900

    feat: 가입시 축하 이메일 테스트송 êµ구현

---
+++ midProject/src/main/java/com/sbspro/midProject/domain/email/service/EmailService.java
@@ -0,0 +1,32 @@
+package com.sbspro.midProject.domain.email.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public void send(String to, String subject, String body){
+        if(to.endsWith("@test.com")) return;
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        try{
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(body);
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
--- midProject/src/main/java/com/sbspro/midProject/member/service/MemberService.java
+++ midProject/src/main/java/com/sbspro/midProject/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.sbspro.midProject.member.service;
 
 import com.sbspro.midProject.base.rsData.RsData;
+import com.sbspro.midProject.domain.email.service.EmailService;
 import com.sbspro.midProject.domain.genFile.entity.GenFile;
 import com.sbspro.midProject.domain.genFile.service.GenFileService;
 import com.sbspro.midProject.member.entity.Member;
@@ -20,6 +21,8 @@ public class MemberService {
     private final MemberRepository memberRepositroy;
     private final PasswordEncoder passwordEncoder;
     private final GenFileService genFileService;
+    private final EmailService emailService;
+
 
     @Transactional
     public RsData<Member> join(String username, String password, String nickname, String email,  MultipartFile profileImg) {
@@ -44,6 +47,8 @@ public class MemberService {
             genFileService.save(member.getModelName(), member.getId(), "common", "profileImg", 0, profileImg);
         }
 
+        sendJoinCompleteMail(member);
+
             return RsData.of("S-1", "회원가입이 완료되었습니다", member);
         }
 
@@ -63,6 +68,10 @@ public class MemberService {
         if(findByUsername(username).isPresent()) return RsData.of("F-1", "%s는 사용중인 아이디입니다.".formatted(username));
 
         return RsData.of("S-1", "%s는 사용 가능한 아이디입니다.".formatted(username));
+    }
+
+    private void sendJoinCompleteMail(Member member) {
+        emailService.send(member.getEmail(),"회원가입이 완료되었습니다.", "회원가입이 완료되었습니다.");
     }
 
     public RsData<String> checkEmailDup(String email) {
